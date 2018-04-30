978d1a78e83e4da59c37dbfdaa2db0bbe6afce7f
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2018-04-30 07:07:15 -0400

    Add solution to p3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 #![feature(test)]
 
+extern crate primes;
 extern crate test;
 use test::Bencher;
 
 mod utils;
 // mod p1;
 mod p2;
+mod p3;
 
 fn main() {
     // p1::fold_sum(10000);
-    p2::solve_takewhile(4000000);
+    // p2::solve_takewhile(4000000);
+    p3::solve3(600851475143);
 }
 
 #[bench]
@@ -30,6 +33,20 @@ fn while_sum(b: &mut Bencher) {
 fn takewhile_sum(b: &mut Bencher) {
     b.iter(|| {
         p2::solve_takewhile(40000000);
+    });
+}
+
+#[bench]
+fn takewhile_filter_sum(b: &mut Bencher) {
+    b.iter(|| {
+        p2::solve_takewhile_filter(40000000);
+    });
+}
+
+#[bench]
+fn takewhile_filter_mutable_sum(b: &mut Bencher) {
+    b.iter(|| {
+        p2::solve_takewhile_filter_mutable(40000000);
     });
 }
 
